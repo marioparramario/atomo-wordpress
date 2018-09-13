7f12130cfc2b872069892fd4e3dd46f5c6011cba
Fabian <fabian.niemann@gmail.com>
2018-09-13 18:19:56 +0200

    Allow configuration of SAVEQUERIES via environment
    Variable name: WP_SAVEQUERIES

--- wp-config.php
+++ wp-config.php
@@ -91,6 +91,7 @@ $table_prefix  = 'wp_';
 define('WP_DEBUG', getenv('WP_DEBUG', true));
 define('WP_DEBUG_LOG', getenv('WP_DEBUG_LOG', true));
 define('WP_DEBUG_DISPLAY', getenv('WP_DEBUG_DISPLAY', true));
+define('SAVEQUERIES', getenv('WP_SAVEQUERIES', true));
 
 
 /* That's all, stop editing! Happy blogging. */
