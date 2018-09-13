48a4fa1824343c44607d6cd55b1e8e145a361c41
Fabian <fabian.niemann@gmail.com>
2018-09-13 18:10:42 +0200

    Enable compose autoloading

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,17 @@
  * @package WordPress
  */
 
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+try {
+	(new Dotenv\Dotenv(__DIR__))->load();
+} catch (\Throwable $e) {
+	error_log($e->getMessage());
+}
+
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', getenv('DB_NAME', true) ?: 'atomo');
