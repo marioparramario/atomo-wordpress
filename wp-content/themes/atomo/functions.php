d178bf3723943b5ca7cb06b25646d3337fe4bfad
Fabian <fabian.niemann@gmail.com>
2018-09-22 00:54:20 +0200

    Use correct @subpackage directive

--- wp-content/themes/atomo/functions.php
+++ wp-content/themes/atomo/functions.php
@@ -4,7 +4,8 @@
  * Functions and constants for Átomo theme.
  *
  * @link    https://developer.wordpress.org/themes/basics/theme-functions/
- * @package atomo
+ * @package WordPress
+ * @subpackage Atomo
  */
 
 if (!function_exists('atomo_setup')) {
