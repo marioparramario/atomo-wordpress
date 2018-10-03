0bdf5dd915c47a374097e7ad0c2ff231aae60a7d
Fabian <fabian.niemann@gmail.com>
2018-10-04 01:12:00 +0200

    Implement custom language switcher

--- wp-content/themes/atomo/inc/template-tags.php
+++ wp-content/themes/atomo/inc/template-tags.php
@@ -8,6 +8,77 @@
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 
+if ( ! function_exists( 'atomo_language_switcher' ) ) {
+	/**
+	 * Display language switcher widget.
+	 *
+	 * @param string|array $class (Optional) Class attributes for widget.
+	 *
+	 * @see https://polylang.wordpress.com/documentation/
+	 */
+	function atomo_language_switcher( $class = '' ) {
+		echo atomo_get_language_switcher( $class );
+	}
+
+	/**
+	 * Generate language switcher widget.
+	 *
+	 * @param string|array $class (Optional) Class attributes to use.
+	 * @return string Rendered language switcher HTML.
+	 */
+	function atomo_get_language_switcher( $class = '' ) {
+
+		if ( ! function_exists( 'pll_the_languages' ) ) {
+			error_log( 'Missing "polylang" plugin' );
+			return;
+		}
+
+		$languages = pll_the_languages( [
+			'display_names_as' => 'slug',
+			'raw' => true,
+		] );
+
+		if ( empty( $languages ) ) {
+			return '';
+		}
+
+		$class = $class ?: [];
+		if ( ! is_array( $class ) ) {
+			$class = explode( ',', $class );
+			foreach ( $class as $name ) {
+				$class[$name] = trim( $name );
+			}
+		}
+
+		if ( ! in_array( 'lang-switcher', $class ) ) {
+			$class[] = 'lang-switcher';
+		}
+
+		$links = [];
+		foreach ($languages as $lang => $info ) {
+
+			$classes = array_merge( ['lang-link'], $info['classes'] );
+			if ( $info['current'] ?? false ) {
+				$classes[] = 'active';
+			}
+
+			$title = locale_get_display_name( $info['locale'] );
+			$abbr = sprintf( '<abbr title="%s">%s</abbr>', esc_attr( $title ), strtoupper( $lang ) );
+			$links[] = sprintf( '<a class="%s" href="%s" rel="alternate" hreflang="%s">%s</a>',
+			                    esc_attr( implode( ' ', $classes ) ),
+			                    esc_url( $info['url'] ),
+			                    $info['slug'],
+			                    $abbr );
+		}
+
+		$nav = implode( '<div class="separator"></div>', $links );
+		$out = sprintf( '<div class="%s">%s</div>', implode( ',', $class ), $nav );
+
+		return $out;
+	}
+}
+
+
 if ( ! function_exists( 'atomo_excerpt' ) ) {
 	/**
 	 * Display a short excerpt of the post's contents.
