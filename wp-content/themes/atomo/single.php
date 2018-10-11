3de0b96b7fdf88b196c1ef4cd842727cbfe69812
Fabian <fabian.niemann@gmail.com>
2018-10-11 21:41:11 +0200

    Clean up handling of  meta-box

--- wp-content/themes/atomo/single.php
+++ wp-content/themes/atomo/single.php
@@ -14,12 +14,12 @@ get_header(); ?>
 
 
 <div class="container flex vertical align-center">
-<?php if ( is_singular() ): ?>
+<?php if ( is_singular() ) : ?>
 	<section class="single singular">
 		<div class="single-container flex-vertical">
-		<?php if ( !have_posts() ): ?>
+		<?php if ( ! have_posts() ) : ?>
 			<p class="no-posts"><?php _e( 'Sorry, no posts matched your criteria.', 'atomo' ); ?></p>
-		<?php else: ?>
+		<?php else : ?>
 			<?php while ( have_posts() ) { the_post(); ?>
 			<article <?php post_class( 'flex-vertical' ); ?> id="post-<?php the_ID(); ?>">
 				<?php $thumb_url = get_the_post_thumbnail_url( get_the_ID(), 'normal' ); ?>
@@ -28,11 +28,12 @@ get_header(); ?>
 					<div class="single-social sticky"></div>
 					<div class="single-text-container flex vertical">
 						<h3 class="single-post-title"><?php the_title(); ?></h3>
-						<?php $meta_value = get_post_meta($post->ID, "author-article", true);
-								if (!empty($meta_value)) {
-									echo '<h4>'. $meta_value .'</h4>';
-								}
-								?>
+						<?php
+							$atomo_author = get_post_meta( $post->ID, 'author-article', true );
+							if ( ! empty( $atomo_author ) ) {
+								echo '<h4>' . esc_html( $atomo_author ) . '</h4>';
+							}
+						?>
 						<span><?php the_date( 'j, F, Y', null, __( ' by', 'atomo' ) ); ?></span>
 						<div class="post-body">
 							<?php the_content(); ?>
@@ -45,9 +46,9 @@ get_header(); ?>
 <?php else: ?>
 	<section class="single">
 		<div class="single-container flex-vertical">
-		<?php if ( !have_posts() ): ?>
+		<?php if ( ! have_posts() ) : ?>
 			<p class="no-posts"><?php _e( 'Sorry, no posts matched your criteria.', 'atomo' ); ?></p>
-		<?php else: ?>
+		<?php else : ?>
 			<?php while ( have_posts() ) { the_post(); ?>
 			<article <?php post_class( 'col-md-12 flex-vertical' ); ?> id="post-<?php the_ID(); ?>">
 				<a class="perma-link" href="<?php echo esc_url( get_permalink() ); ?>">
